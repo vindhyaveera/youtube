c0028c3cf6e6916c2dc6d2f372f26ed09b3a58ef
vindhyaveera <vindhyadevi12@gmail.com>
2024-11-27 11:18:59 +0530

    youtube

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,25 @@
 // src/app/store.js
 
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Defaults to localStorage for web
 import videoReducer from '../features/videos/videoSlice';
 
+// Persist configuration for videos slice
+const persistConfig = {
+  key: 'videos',
+  storage,
+};
+
+// Wrap the videoReducer with persistReducer
+const persistedVideoReducer = persistReducer(persistConfig, videoReducer);
+
+// Create the store
 export const store = configureStore({
   reducer: {
-    videos: videoReducer,
+    videos: persistedVideoReducer,
   },
 });
+
+// Create a persistor instance
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./app/store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -14,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
